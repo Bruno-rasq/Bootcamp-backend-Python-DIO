1860d3a2524165642740a401c701c7f50e98d169
Rasquinha_ <116691516+Bruno-rasq@users.noreply.github.com>
2024-04-06 14:05:12 +0000

    doc: adicionando tuplas

---
+++ estruturas/tuplas/declaracao.py
@@ -0,0 +1,24 @@
+frutas = (
+  "laranja",
+  "pera",
+  "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
+
+#acesso direto 
+print(frutas[0])  # laranja
+print(frutas[2])  # uva
+
+#indices negativos
+print(frutas[-1])  # uva
+print(frutas[-3])  # laranja
+
