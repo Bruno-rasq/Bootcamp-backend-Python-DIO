e19c3f6407aacd36bd0c400aaf5db8438637aa67
Rasquinha_ <116691516+Bruno-rasq@users.noreply.github.com>
2024-04-06 03:17:37 +0000

    Doc: Estrutura de dados - list

---
+++ estruturas/listas/metodos.py
@@ -0,0 +1,66 @@
+# acesso direto aos elementos da lista
+
+frutas = ["maçã", "laranja", "uva", "pera"]
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+
+# indices negativos
+
+print(frutas[-1])  # pera
+print(frutas[-3])  # laranja
+
+
+# matriz
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"]
+]
+
+print(matriz[0])  # [1, "a", 2]
+print(matriz[0][0])  # 1
+print(matriz[0][-1])  # 2
+print(matriz[-1][-1])  # "c"
+
+
+# fatiamento
+
+lista = ["p", "y", "t", "h", "o", "n"]
+
+print(lista[2:])  # ["t", "h", "o", "n"]
+print(lista[:2])  # ["p", "y"]
+print(lista[1:3])  # ["y", "t"]
+print(lista[0:3:2])  # ["p", "t"]
+print(lista[::])  # ["p", "y", "t", "h", "o", "n"]
+print(lista[::-1])  # ["n", "o", "h", "t", "y", "p"]
+
+
+# iterar lista
+
+carros = ["gol", "celta", "palio"]
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+
+# compreesao de lista
+
+# Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+
+# ------------------------
